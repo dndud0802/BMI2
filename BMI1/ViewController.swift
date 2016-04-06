bd87336f8214cce34d3236946cdcd2c43a806cdd
wooyoung <cwy0802@naver.com>
2016-04-06 14:45:56 +0900

    0406 finished

--- BMI1/ViewController.swift
+++ BMI1/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BMILabel: UILabel!
+    
+    @IBOutlet weak var HeightTextField: UITextField!
+    
+    @IBOutlet weak var WeightTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
